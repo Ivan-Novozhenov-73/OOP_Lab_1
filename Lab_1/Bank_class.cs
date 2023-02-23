b611a3432ae7ce5f6f9f9298eca9f570c553b9a6
Ivan Novozhenov <ivannovozenov73@gmail.com>
2023-02-23 11:45:24 +0300

    Создание класса Bank, добавление полей. Создание метода print() для вывода информации об объекте

---
+++ Lab_1/Bank_class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_namespace
+{
+    internal class Bank
+    {
+        public string _bankName;            // название банка
+        private int _countDeposits;         // кол-во депозитов
+        private float _amountDeposits;      // сумма депозитов
+        private float _interestRate;        // процентная ставка
+        private int _numClients;            // кол-во клиентов
+        private int _yearFoundation;        // год основания
+        private string _location;           // расположение
+
+        public Bank()
+        {
+            _bankName = "Банк";
+            _countDeposits = 0;
+            _amountDeposits = 0;
+            _interestRate = 0;
+            _numClients = 0;
+            _yearFoundation = 1970;
+            _location = "Город";
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"Название банка: {_bankName}");
+            Console.WriteLine($"Количество депозитов: {_countDeposits}");
+            Console.WriteLine($"Сумма депозитов: {_amountDeposits}");
+            Console.WriteLine($"Процентная ставка: {_interestRate}");
+            Console.WriteLine($"Количество клиентов: {_numClients}");
+            Console.WriteLine($"Год основания: {_yearFoundation}");
+            Console.WriteLine($"Расположение: {_location}");
+        }
+    }
+}
---
+++ Lab_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Bank_namespace;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 1. Группа 21ВП2");
+            Console.WriteLine("Бригада 6. Антонов, Новоженов \n");
+
+            Bank bank = new Bank();
+            bank.print();
+        }
+    }
+}
